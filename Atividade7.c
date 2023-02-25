434c8d2851bea4b460813a2d7b0f37b19fde92e1
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2023-02-25 19:50:49 -0300

    conversão fahrenheit para celsius

---
+++ Atividade7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	float f, c;
+	
+	printf("Digite um valor em Fahrenheit: \n");
+	scanf("%f", &f);
+	
+	c = (f-32)/1.8;
+	
+	printf("O valor em Celsius e: %f", c);
+}
