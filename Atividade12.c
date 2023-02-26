40fea6ff002d1edc6c1bce2c0a71bc147865b642
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2023-02-25 22:18:39 -0300

    Conversão milhas para kilômetros

---
+++ Atividade12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	float mi, km;
+	
+	printf("Digite o numero desejado em milhas: \n");
+	scanf("%f", &mi);
+	
+	km = mi*1.61;
+	
+	printf("O valor em km e: %f", km);
+}
