4b5e2bce16b67d38b0e4b05e4e225f233c1d30cf
rafamaiadev <rafa.maia27@hotmail.com>
2023-03-06 15:37:58 -0300

    Desenvolvendo elementos de uma PA

---
+++ Lista03-Estrutura_De_Repeticao_For_E_While/Atividade06.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int i, a1, r, n, soma = 0;
+	
+	printf("Digite o primeiro termo da PA: \n");
+	scanf("%d", &a1);
+	
+	printf("Digite a razao da PA: \n");
+	scanf("%d", &r);
+	
+	printf("Os 10 primeiros elementos da PA sao: \n");
+	for(i = 0; i<10; i++) {
+		
+		n = a1+i*r;
+		
+		soma += n;
+		
+		printf("%d \n", n);
+	}
+	
+	
+	
+	printf("A soma dos elementos e: %d", soma); 
+}
